0a45deb807de42a0d91726b116fd3b9d4ee1a9ba
Just Friends <brino@pop-os.localdomain>
2022-06-22 13:54:37 -0300

    test: initial app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { GraphQLModule } from './graphql.module';
+import { fakeAsync } from '@angular/core/testing';
+import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent]
-    }).compileComponents();
-  });
+  beforeEach(fakeAsync(async () => {
+    await render(AppComponent, {
+      imports: [GraphQLModule, HttpClientTestingModule]
+    });
+  }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ng-event-platform'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-event-platform');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'ng-event-platform app is running!'
-    );
+    expect(AppComponent).toBeTruthy();
   });
 });

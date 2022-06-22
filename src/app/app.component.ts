d8111a587128cc729e99184870bb83dfcdb46800
Just Friends <brino@pop-os.localdomain>
2022-06-21 23:00:16 -0300

    feat: integrated graphql and made first integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-event-platform';
+  public data: any;
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit(): void {
+    this.apollo.
+      watchQuery({
+        query: gql`
+          {
+            lessons {
+              id
+              title
+            }
+          }
+        `
+      }).valueChanges
+        .pipe(
+          tap(response => console.log('api response: ', response)),
+          map(response => response.data))
+          .subscribe((response:any) => {
+            this.data = response.lessons
+          })
+  }
 }
